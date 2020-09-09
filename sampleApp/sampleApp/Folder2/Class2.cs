d628cfba5a0767e9c7d35de2843f7bedb5bf6e50
kouhei.shimode <kouhei.shimode@m.agrex.co.jp>
2020-09-09 15:46:51 +0900

    [feat][]手順書作成用のサンプルプロジェクトをコミット

---
+++ sampleApp/sampleApp/Folder2/Class2.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sampleApp.Folder2
+{
+    class Class2
+    {
+    }
+}
---
+++ sampleApp/sampleApp/Foldre1/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sampleApp.Foldre1
+{
+    class Class1
+    {
+    }
+}
